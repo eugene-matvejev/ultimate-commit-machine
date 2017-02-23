56c3b2309a302904b68b9de156823862fa690c20
Eugene Matvejevs <eugene.matvejev@gmail.com>
2017-02-23 16:27:16 +0000

    1-1 test commit attack

--- ultimate-commit-machine.sh
+++ ultimate-commit-machine.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 COPY="${PWD}/README.md";
+DESTROY="${PWD}/README.md.attack";
 
 for i in {1..499000}
 do
+    cp -f $COPY $DESTROY;
     git add .
     git commit -m "${i}-1 test commit attack";
 
-    git remove COPY;
+    rm -rf $DESTROY;
+    git add .
     git commit -m "${i}-2 test commit attack";
 done
