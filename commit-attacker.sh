1c6b5d0486cd21d4267f7ddd777a9ce76aa77dca
Eugene Matvejevs <eugene.matvejev@gmail.com>
2017-02-23 16:21:12 +0000

    1-1 test commit attack

--- commit-attacker.sh
+++ commit-attacker.sh
@@ -6,10 +6,11 @@ DESTROY="${PWD}/README.md.attack";
 for i in {1..499000}
 do
     cp -f $COPY $DESTROY;
-    git add .
-    git commit -m "${i}-1 test commit attack";
+    echo "{i}";
+    git add . &
+    git commit -m "${i}-1 test commit attack" &;
 
     rm -rf $DESTROY;
-    git add .
-    git commit -m "${i}-2 test commit attack";
+    git add . &
+    git commit -m "${i}-2 test commit attack" &;
 done
